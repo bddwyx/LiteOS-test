d8572d3471fd37f63b3f6cf49b23fc8db27fcfe3
bddwyx <56229174+bddwyx@users.noreply.github.com>
2021-06-08 16:07:01 +0800

    PWM beeper

--- Drivers/Beeper.c
+++ Drivers/Beeper.c
@@ -3,7 +3,25 @@
 void BeepInit(){
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOK);						//Enable PortK
     while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOK));			//Wait for the GPIO moduleK ready
-    GPIOPinTypeGPIOOutput(GPIO_PORTK_BASE, GPIO_PIN_5);			//Set PK5 as Output pin
+    GPIOPinTypeGPIOOutput(GPIO_PORTK_BASE, GPIO_PIN_5);			//Set PK5 as Output pin*/
+
+    GPIOPinConfigure(GPIO_PK5_M0PWM7);
+    GPIOPinTypePWM(GPIO_PORTK_BASE, GPIO_PIN_5);
+
+    SysCtlPWMClockSet(SYSCTL_PWMDIV_1);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM0);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_PWM0));
+
+    PWMGenConfigure(PWM0_BASE, PWM_GEN_3,
+                    PWM_GEN_MODE_DOWN | PWM_GEN_MODE_NO_SYNC);
+
+    PWMGenPeriodSet(PWM0_BASE, PWM_GEN_3, 40000);
+    PWMPulseWidthSet(PWM0_BASE, PWM_OUT_7, 20000);
+
+    PWMGenEnable(PWM0_BASE, PWM_GEN_3);
+
+    PWMOutputState(PWM0_BASE, PWM_OUT_7_BIT, true);
+
 }
 
 void Beep(){
@@ -12,6 +30,6 @@ void Beep(){
         GPIOPinWrite(GPIO_PORTK_BASE, GPIO_PIN_5,
                      ~GPIOPinRead(GPIO_PORTK_BASE, GPIO_PIN_5)
         );
-        SysCtlDelay(120000000 / (1048 * 3));
+        //SysCtlDelay(120000000 / (1048 * 3));
     }
 }
--- Drivers/Beeper.h
+++ Drivers/Beeper.h
@@ -5,8 +5,11 @@
 #include <stdbool.h>
 
 #include "hw_memmap.h"
+#include "pin_map.h"
 #include "gpio.h"
 #include "sysctl.h"
+#include "hw_pwm.h"
+#include "pwm.h"
 
 void BeepInit(void);
 void Beep(void);
